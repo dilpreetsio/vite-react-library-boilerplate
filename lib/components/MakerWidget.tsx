79d1ce87f76d5231635f94d95260396609b673b1
dilpreetsio <singhsince94@gmail.com>
2023-12-11 21:46:43 +0530

    basic

---
+++ lib/components/MakerWidget.tsx
@@ -0,0 +1,10 @@
+// type MakerWidgetProps = {
+//   name?: string;
+//   socials?: string[];
+//   projects?: string[];
+// };
+import styles from "./style.module.css";
+
+export function MakerWidget() {
+  return <div className={styles.makerWidget}>MakerWidget</div>;
+}
